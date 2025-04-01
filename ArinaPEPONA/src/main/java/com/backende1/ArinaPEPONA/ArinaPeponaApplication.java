992b72d6b437483b80a736cc0219395f94fd44ea
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2025-04-01 10:59:13 -0300

    ahora si

---
+++ ArinaPEPONA/src/main/java/com/backende1/ArinaPEPONA/ArinaPeponaApplication.java
@@ -0,0 +1,13 @@
+package com.backende1.ArinaPEPONA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArinaPeponaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArinaPeponaApplication.class, args);
+	}
+
+}
---
+++ ArinaPEPONA/src/test/java/com/backende1/ArinaPEPONA/ArinaPeponaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backende1.ArinaPEPONA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArinaPeponaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
